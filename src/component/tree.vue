ef678f8e6e324d82a63088ffc7073f1624e8417d
v_xijiangwu <v_xijiangwu@tencent.com>
2020-11-02 20:28:53 +0800

    save

--- src/component/tree.vue
+++ src/component/tree.vue
@@ -1,10 +1,66 @@
 <template>
-    <a-tree></a-tree>
+  <a-directory-tree multiple default-expand-all @select="onSelect" @expand="onExpand">
+    <a-tree-node
+      v-for="(item,index) of data"
+      :key="index"
+      :title="item.label"
+    >
+
+    </a-tree-node>
+  </a-directory-tree>
 </template>
 
 <script>
 export default {
   name: 'tree',
+  props: {
+    data: {
+      type: Array,
+      default: () => [{
+        label: '一级 1',
+        children: [{
+          label: '二级 1-1',
+          children: [{
+            label: '三级 1-1-1',
+          }],
+        }],
+      }, {
+        label: '一级 2',
+        children: [{
+          label: '二级 2-1',
+          children: [{
+            label: '三级 2-1-1',
+          }],
+        }, {
+          label: '二级 2-2',
+          children: [{
+            label: '三级 2-2-1',
+          }],
+        }],
+      }, {
+        label: '一级 3',
+        children: [{
+          label: '二级 3-1',
+          children: [{
+            label: '三级 3-1-1',
+          }],
+        }, {
+          label: '二级 3-2',
+          children: [{
+            label: '三级 3-2-1',
+          }],
+        }],
+      }],
+    },
+  },
+  methods: {
+    onSelect(keys, event) {
+      console.log('Trigger Select', keys, event);
+    },
+    onExpand() {
+      console.log('Trigger Expand');
+    },
+  },
 };
 </script>
 
--- src/editor-tree-store.js
+++ src/editor-tree-store.js
@@ -1,11 +1,16 @@
 import { FileSystem } from 'file-api';
 
-const fs = new FileSystem({ treePrefix: 'trees', pathPrefix: 'path' });
+let fs = null;
 // fs 用于持久化数据，最新变量从state里面获取
 export default {
   state: {
+    namespace: '',
     fileList: [],
     editorContent: {},
+  },
+  initFs(namespace) {
+    this.state.namespace = namespace;
+    fs = new FileSystem({ treePrefix: `trees-${namespace}`, pathPrefix: `path-${namespace}` });
   },
   // 初始化文件列表、会清除之前的缓存
   initFileList: async (fileList) => {
@@ -16,8 +21,31 @@ export default {
     await Promise.all(p);
   },
   // 设置编辑器内容
-  writeFile(path, content) {
+  async writeFile(path, content) {
     this.state.editorContent[path] = content;
-    fs.writeFile(path, content);
+    await fs.writeFile(path, content);
+  },
+  async appendFile(path, data) {
+    this.state.editorContent[path] = this.state.editorContent[path] + data;
+    await fs.appendFile(path, data);
+  },
+  async readFile(path, options) {
+    return fs.readFile(path, options);
+  },
+  async rename(oldPath, newPath) {
+    if (this.state.fileList[newPath]) {
+      throw new Error('文件名重复');
+    }
+    await fs.rename(oldPath, newPath);
+    this.state.fileList[newPath] = this.state.fileList[oldPath];
+  },
+  async mkdir(path, options) {
+    return fs.mkdir(path, options);
+  },
+  async rmdir(path) {
+    return fs.rmdir(path);
+  },
+  async readdir(path) {
+    return fs.readdir(path);
   },
 };
