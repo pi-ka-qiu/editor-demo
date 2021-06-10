a6d919c7ec3c689b8c7642bc8321e056a6b33e7f
ge-ge <WXJ907468952@outlook.com>
2021-06-10 16:57:10 +0800

    save

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,11 @@ module.exports = {
     '@vue/airbnb',
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'off' : 'off',
-    'max-len': ['error', { code: 200 }],
+    "no-console": process.env.NODE_ENV === "production" ? "off" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "off" : "off",
+    "max-len": ["error", { code: 200 }],
+    "max-lines-per-function": ["error", { max: 50 }],
+    "max-depth": ["error", 4],
   },
   parserOptions: {
     parser: 'babel-eslint',
--- src/component/editor.vue
+++ src/component/editor.vue
@@ -9,6 +9,7 @@ import '../lib/browser-editor/browser-editor.scss';
 export default {
   name: 'editor',
   mounted() {
+    // eslint-disable-next-line no-unused-vars
     const editor = new Editor(this.$el, { placeholder: '输入些什么' });
   },
   methods: {
